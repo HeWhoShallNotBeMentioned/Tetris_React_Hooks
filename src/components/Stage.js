ea7a01eb687f07b42787f939a7daed54d44b6c0a
Chris Underwood <cunderwoodmn@gmail.com>
2019-09-21 22:06:30 -0700

    add code to create a stage to have cell fields appear on the board

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -6,7 +6,7 @@ const Stage = ({ stage }) => (
   <div>
     { stage && stage.length > 0  ? 
     stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} />))
-  : 'There is no data yet.' }
+  : 'cell' }
   </div>
 );
 
